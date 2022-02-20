1215b916ab73a35ba55580980a8f36312f028924
ttok14 <ttok14@naver.com>
2022-02-20 09:44:32 +0900

    - 기존 Direct3D 초기화 주석 보충, Shader 파일 / 컴파일 추가

--- Direct3D11_ToyProject/Direct3D11_ToyProject.cpp
+++ Direct3D11_ToyProject/Direct3D11_ToyProject.cpp
@@ -9,6 +9,8 @@
 #include <dxgi.h>											// DirectX driver interface
 #include <d3dcompiler.h>								// shader compiler
 
+// Direct3D11 라이브러리 코드에서 참조하기
+//		=> 프로젝트 속성에서 링크로 설정하는 방법도 있음. 
 #pragma comment( lib, "user32" )					// link against the win32 library
 #pragma comment( lib, "d3d11.lib" )				// direct3D library
 #pragma comment( lib, "dxgi.lib" )					// directx graphics interface
@@ -16,7 +18,10 @@
 
 #define MAX_LOADSTRING 100
 
+// ** 함수 전방선언 **//
 void InitializeDirect3D();
+void SetupShader();
+HRESULT CompileShader(LPCWSTR shaderFileName, LPCSTR entryPoint, LPCSTR target, UINT flags, ID3DBlob** ppCode, ID3DBlob** ppErrorBlob);
 
 // 전역 변수:
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
@@ -29,9 +34,17 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+// Direct3D Device 는 Direct3D 로 구동시키는 애플리케이션에서 최소 하나가 필요
+// 가장 Driver 에 가까운 존재 , Direct3D 의 '본체' 
 ID3D11Device* pDevice = NULL;
+// Device Context 즉 문맥임. 그래서 보면 '현재 상태' 관련된 메서드들이 많음. 
+// '현재 상태' 를 어떻게 설정하고 그 상태로 드로우를 하고 등.. 
 ID3D11DeviceContext* pDeviceContext = NULL;
+// SwapChain 은 모니터 화면에 보이는 FrontBuffer (사실은 Texture2D 임) 와 그 뒤에서 
+// 다음 Frame 위해 드로잉이 되고 있는 BackBuffer 등 렌더링 관련 여러 Buffer 들의 그룹을 
+// SwapChain 이라함 
 IDXGISwapChain* pSwapChain = NULL;
+// ??
 ID3D11RenderTargetView* pRenderTargetView = NULL;
 
 HWND g_hWnd;
@@ -60,6 +73,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DIRECT3D11TOYPROJECT));
 
 	InitializeDirect3D();
+	SetupShader();
 
 	MSG msg = {};
 
@@ -84,18 +98,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	return (int)msg.wParam;
 }
 
+// Direct3D 기본 초기화 함수
 void InitializeDirect3D()
 {
 	DXGI_SWAP_CHAIN_DESC swapChainDesc = { 0 };
 	swapChainDesc.BufferDesc.RefreshRate.Numerator = 0;
 	swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
-	/// DXGI_FORMAT_B8G8R8A8_UNORM 는 Gamma Correction 적용 X
-	///		=> DXGI_FORMAT_B8G8R8A8_UNORM_SRGB 로 대체 가능
+	// DXGI_FORMAT_B8G8R8A8_UNORM 는 Gamma Correction 적용 X
+	//		=> Gamma Correction 적용을 위해서는 
+	//			  DXGI_FORMAT_B8G8R8A8_UNORM_SRGB 로 대체 가능
+	//		=> Gamma Correction 이란 sRGB 색상 공간에서 본래의 색상보다 
+	//			밝아진 상태로 저장된 이미지들을 원래로 돌리기 위해 특정한
+	//			감마 공식을 색상들에 적용하는 것임 
 	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
 	swapChainDesc.SampleDesc.Count = 1;
 	swapChainDesc.SampleDesc.Quality = 0;
 	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	// BackBuffer 개수 . 일반적으로 FrontBuffer 1 개 BackBuffer 1 개로 SwapChain 구성 
+	// [BackBuffer 개수] . 일반적으로 FrontBuffer 1 개 BackBuffer 1 개로 SwapChain 구성 
 	swapChainDesc.BufferCount = 1;
 	swapChainDesc.OutputWindow = g_hWnd;
 	swapChainDesc.Windowed = true;
@@ -106,11 +125,18 @@ void InitializeDirect3D()
 #if defined(DEBUG) || defined (_DEBUG)
 	flags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
+
+	// Direct3D Device 생성하고 SwapChain 까지 생성.
 	HRESULT hr = D3D11CreateDeviceAndSwapChain(
 		NULL,
-		D3D_DRIVER_TYPE_HARDWARE, // Hardware => Software => Reference 순으로 속도 차이발생 
+		// Hardware => Software => Ref 순으로 속도 차이발생 
+		//		=> Ref 는 거의 못사용할 정도로 느리다함 .
+		D3D_DRIVER_TYPE_HARDWARE,
 		NULL,
 		flags,
+		// TODO : FeatureLevel 자료 검색해서 추가할것 
+		//		=> 정확하진 않지만 D3D9 에서는 Caps 어쩌거 있었던거같은데
+		//			그게 너무 복잡하고 해서 이거로 대체됐다함 . 
 		NULL,
 		0,
 		D3D11_SDK_VERSION,
@@ -128,9 +154,9 @@ void InitializeDirect3D()
 	// when the program is built in debug mode. If all went well, 
 	// you should be able to now compile and run it, without an assertion 
 	// triggering. Otherwise check the parameter values carefully. 
-	// The output images from Direct3D are called Render Targets. 
-	// We can get a view pointer to ours now, by fetching it from our swap chain.
 	ID3D11Texture2D* framebuffer;
+	// Direct3D 에서 드로잉 결과물 이미지들은 Render Targets 라고 불림 
+	// SwapChain 에서 View Pointer 를 가져올 수가 있음.
 	hr = pSwapChain->GetBuffer(
 		0,
 		__uuidof(ID3D11Texture2D),
@@ -140,10 +166,70 @@ void InitializeDirect3D()
 	assert(SUCCEEDED(hr));
 
 	hr = pDevice->CreateRenderTargetView(framebuffer, 0, &pRenderTargetView);
-	
-	assert(SUCCEEDED(hr));
-
+	// COM 오브젝트들은 객체의 할당에 있어서 Reference Counting 을 사용하기 때문에 
+	// 위에서 발생한 Ref 카운트 증가를 하나 깍아주기 위해 Release() 를 호출함. 
 	framebuffer->Release();
+
+	assert(SUCCEEDED(hr));
+}
+
+void SetupShader()
+{
+	UINT flags = D3DCOMPILE_ENABLE_STRICTNESS;
+
+#if defined ( DEBUG ) || defined (  _DEBUG ) 
+	flags |= D3DCOMPILE_DEBUG;
+#endif 
+
+	// Blob 은 Binary Large Object 를 의미
+	ID3DBlob* pVsBlob = NULL;
+	ID3DBlob* pPsBlob = NULL;
+	ID3DBlob* errorBlob = NULL;
+
+	// VertexShader, PixelShader 를 Compile 해서 blob(Big binary object) 형태로 캐싱
+	//			=> D3DCompileFromFile 메서드가 수행 
+	LPCWSTR shaderFileName = L"shaders.hlsl";
+
+	// Vertex Shader 컴파일
+	CompileShader(L"shaders.hlsl", "vs_main", "vs_5_0", flags, &pVsBlob, &errorBlob);
+
+	// Pixel Shader 컴파일 
+	CompileShader(L"shaders.hlsl", "ps_main", "ps_5_0", flags, &pPsBlob, &errorBlob);
+}
+
+HRESULT CompileShader(LPCWSTR shaderFileName, LPCSTR entryPoint, LPCSTR target, UINT flags, ID3DBlob** ppCode, ID3DBlob** ppErrorBlob)
+{
+	// Pixel Shader 컴파일 
+	HRESULT hr = D3DCompileFromFile(
+		shaderFileName,
+		nullptr,
+		D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		entryPoint, // 해당 Shader 파일 안에 정의해둔 Vertex / Pixel Shader 함수와 이름이 일치해야함 
+		target,
+		flags,
+		0,
+		ppCode,
+		ppErrorBlob
+	);
+
+	// 컴파일 실패 처리
+	if (FAILED(hr))
+	{
+		if (ppErrorBlob)
+		{
+			OutputDebugStringA((char*)(*ppErrorBlob)->GetBufferPointer());
+			(*ppErrorBlob)->Release();
+		}
+
+		if (ppCode)
+		{
+			(*ppCode)->Release();
+		}
+
+		assert(false);
+	}
+
+	return hr;
 }
 
 //
---
+++ Direct3D11_ToyProject/shaders.hlsl
@@ -0,0 +1,22 @@
+/* Vertex 속성들. Vertex Shader 의 Input 값으로 받게된다 */
+struct vs_in {
+	float3 position_local : POS;
+};
+
+/* 
+	Vertex Shader 의 결과값. Pixel Shader 의 Input 값임. 보통 Pixel Shader 로 오기전 
+	Rasterizer 에 의해 보간 (Interpolation) 처리 됨  
+*/
+struct vs_out {
+	float4 position_clip : SV_POSITION;
+};
+
+vs_out vs_main(vs_in input) {
+	vs_out output = (vs_out)0; // zero the memory first
+	output.position_clip = float4(input.position_local, 1.0);
+	return output;
+}
+
+float4 ps_main(vs_out input) : SV_TARGET {
+	return float4( 1.0, 0.0, 1.0, 1.0 ); 
+}
