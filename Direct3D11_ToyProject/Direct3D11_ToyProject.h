b6b8bc6b83c10ed4df64d2db946c92272b2da13a
ttok14 <ttok14@naver.com>
2022-03-06 14:47:58 +0900

    Direct3D 코드 대부분 ToyMain 으로 이동, g_hWnd 전역화(extern), Vertex/Index Buffer Cube 모양으로 수정 등..
    Done
    
    - 기존에 Direct3D11_ToyProject.cpp (Main Entry) 에 전부 위치했던 Direct 관련 처리 ToyMain 으로 이동
    - Vertex / Index Buffer Cube 모양으로 수정 및 float3 으로 되어있던 부분 DirectXMath Library 에 있는 DirectX namespace 에 있는 XMFLOAT3 으로 수정 (SIMD performance)
    - g_hWnd 를 extern 으로 전역 변수 만듬 (Direct3D11_ToyProject.h 에 위치)
    
    TODO
    - VertexBuffer , IndexBuffer 생성 부분 클래스화
    - World, ViewProjection Transformation 적용
    - Shader Constant buffer Usage

--- Direct3D11_ToyProject/Direct3D11_ToyProject.h
+++ Direct3D11_ToyProject/Direct3D11_ToyProject.h
@@ -1,3 +1,17 @@
-#pragma once
+﻿#pragma once
 
 #include "resource.h"
+
+// extern 이란?
+
+//		=> 참고 : https://stackoverflow.com/questions/10422034/when-to-use-extern-in-c
+// - 쉽게 말하면 서로 다른 File 들끼리 (module) Variable 에 접근애 사용할 수 있게
+//		Global Variable 로 만들어 주는 keyword 
+
+//	- 이 위치에서 최초 변수를 선언.
+//			=> extern 으로 선언 
+//	- 이 변수를 참조할 다른 File 들에서는 해당 변수의 '존재(exsistence)' 만을 Compiler 에게
+//		알려주기 위해 Function 안이 아닌 외부에 해당 Variable 재선언후 사용 가능 (extern 없이, e.g. HWND g_hWnd)
+//	- 원리는 Compiler 가 Compile 을 할때 해당 Variable 이 extern 이라면 선언까지만 확인 후, 
+//		Linker 가 Link 를 할때 실제로 참조하는 곳을 Linking 함. 
+extern HWND g_hWnd;
